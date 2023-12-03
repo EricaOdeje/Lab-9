abcc37d64bb5dc036356637c0b6a3451056ac1f3
Erica Odeje <72512702+EricaOdeje@users.noreply.github.com>
2023-12-03 23:49:05 +0000

    Update read.js

--- src/components/read.js
+++ src/components/read.js
@@ -6,9 +6,6 @@ import axios from "axios";
 {/* I added axios to our project by npm install axios, axios is a Promise based HTTP client.*/ }
 
 function Read() {
-
-
-
     const [data, setData] = useState([]);
     {/*useEffect is a React Hook that lets you synchronize a component with an external system.*/ }
     useEffect(() => {
@@ -19,7 +16,6 @@ function Read() {
                     setData(response.data);
                 }
             )
-
             .catch(
                 (error) => {
                     console.log(error);
@@ -27,14 +23,17 @@ function Read() {
             );
     }, []);
 
+    
+{/* This function reloads data by making another HTTP GET request. */ }
     const ReloadData = (e) =>{
         axios.get('http://localhost:4000/api/books')
+        {/* If the request is successful, update the component state with the received data. */ }
         .then(
             (response) => {
                 setData(response.data);
             }
         )
-
+        {/*  If there's an error in the HTTP request, log the error to the console. */ }
         .catch(
             (error) => {
                 console.log(error);
@@ -45,6 +44,7 @@ function Read() {
     return (
         <div>
 
+{/* Rendering the component with a header and passing the book data and reload function to the 'Book' component. */ }
             <h2>Hello from my Read component</h2>
             <Book myBook={data} Reload={ReloadData}></Book>
         </div>
